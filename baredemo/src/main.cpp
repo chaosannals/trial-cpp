8b91b088c6c90c3b40b90945f61f0869b01a4e24
chaosannals <chaosannals@outlook.com>
2022-07-07 14:12:33 +0800

    save

--- baredemo/src/main.cpp
+++ baredemo/src/main.cpp
@@ -1,3 +1,4 @@
+#include <memory>
 #include <elements.hpp>
 #include <matoya.h>
 #include <bcrypt.h>
@@ -21,21 +22,21 @@ int main(int argc, char* argv[])
     app _app(argc, argv, "Child Window", "com.cycfi.child-window");
     window _win(_app.name());
     _win.on_close = [&_app]() { _app.stop(); };
-    window _win2(_app.name());
 
     view view_(_win);
 
     char a[] = "1111232323";
-    char* b = new char[1024];
-    MTY_BytesToBase64("1111232323", sizeof(a), b, 1024);
+    auto b = std::unique_ptr<char[]>(new char[1024]);
+    MTY_BytesToBase64(a, sizeof(a), b.get(), 1024);
+
+    auto cw1 = make_child_window({ 10, 10, 300, 200 }, b.get());
+    
 
     view_.content(
-        make_child_window({ 10, 10, 300, 200 }, b),
+        cw1,
         make_child_window({ 60, 60, 350, 250 }, "Child Window 2"),
         background
     );
-
-    delete[] b;
 
     _app.run();
     return 0;
--- readme.md
+++ readme.md
@@ -1,6 +1,24 @@
 # exert elements
 
 ```bash
+# 克隆项目并初始化子模块，一般用于构建静态库。
+git clone --recurse-submodules  https://github.com/cycfi/elements.git
+```
+
+```bash
+# 新建项目时，引入子模块 elements 及其依赖。
+git submodule add https://github.com/cycfi/elements.git
+
 git submodule init
 git submodule update --init --recursive
+```
+
+
+.gitmodules 加入： ignore = dirty  忽略改动
+
+```ini
+[submodule "sshclient/lib/elements"]
+	path = sshclient/lib/elements
+	url = https://github.com/cycfi/elements.git
+	ignore = dirty
 ```

4d5ab56193c2109d7dae03d035e6a46aede386e0
chaosannals <chaosannals@outlook.com>
2022-10-22 17:44:15 +0800

    merge elements/main

---
+++ elementsdemo/elementsdemo/sshclient/src/view/main_panel.cpp
@@ -0,0 +1,59 @@
+﻿#include "main_panel.h"
+#include <cmath>
+
+using namespace cycfi::elements;
+
+main_panel::main_panel() :
+	_mouse_over(false),
+	_color(0.2, 0.2, 0.2)
+{
+
+}
+
+void main_panel::draw(const context& ctx)
+{
+	ctx.canvas.fill_style(_color);
+	ctx.canvas.fill_rect(ctx.bounds);
+}
+
+bool main_panel::click(
+	const context &ctx,
+	mouse_button btn
+) {
+	if (btn.state == mouse_button::left) {
+		_color.blue = std::fmodf(_color.blue + 0.1f, 1.0f);
+		// 刷新，示例好像不推荐在数据事件里面刷新。
+		//ctx.view.refresh(ctx.bounds);
+	}
+	return true;
+}
+
+/// <summary>
+/// 此方法目前没看出作用。
+/// </summary>
+/// <param name="ctx"></param>
+/// <param name="track_info"></param>
+void main_panel::keep_tracking(
+	const context& ctx,
+	tracker_info& track_info
+) {
+	ctx.view.refresh(ctx.bounds);
+}
+
+bool main_panel::cursor(
+	context const& ctx, point p,
+	cursor_tracking status
+) {
+	switch (status)
+	{
+	case cursor_tracking::hovering:
+	case cursor_tracking::entering:
+		_mouse_over = true;
+		// 示例居然在 hovering 刷新，不知道是否会过于频繁。
+		ctx.view.refresh(ctx.bounds);
+		break;
+	case cursor_tracking::leaving:
+		_mouse_over = false;
+	};
+	return false;
+}
---
+++ elementsdemo/elementsdemo/sshclient/src/view/main_panel.h
@@ -0,0 +1,23 @@
+﻿#ifndef EXERT_SSHCLIENT_VIEW_MAIN_WINDOW_H
+#define EXERT_SSHCLIENT_VIEW_MAIN_WINDOW_H
+
+#include <elements.hpp>
+#include <infra/support.hpp>
+
+
+class main_panel :
+	public cycfi::elements::tracker<>
+{
+	bool _mouse_over;
+	cycfi::elements::color _color;
+public:
+	main_panel();
+
+	// 元素绘制重写
+	void draw(const cycfi::elements::context &ctx) override;
+	bool click(const cycfi::elements::context &ctx, cycfi::elements::mouse_button btn) override;
+	void keep_tracking(const cycfi::elements::context &ctx, cycfi::elements::tracker_info& track_info) override;
+	bool cursor(const cycfi::elements::context &ctx, cycfi::elements::point p, cycfi::elements::cursor_tracking status);
+};
+
+#endif

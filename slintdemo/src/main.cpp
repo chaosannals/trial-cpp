bbcda2e1155c0bd10f1d306ce2aa152f18f3366b
chaosannals <chaosannals@outlook.com>
2023-04-07 15:50:32 +0800

    save

--- slintdemo/src/main.cpp
+++ slintdemo/src/main.cpp
@@ -1,7 +1,13 @@
 #include "slintdemo_ui.h"
+#ifdef WIN32
+#include <Windows.h>
+#endif
 
 int main(int argc, char** argv)
 {
+#ifdef WIN32
+    FreeConsole();
+#endif
     auto hello_world = HelloWorld::create();
     hello_world->set_my_label("Hello from C++");
     // Show the window and spin the event loop until the window is closed.

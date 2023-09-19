340a8c69779c13b870d57453a98cf17f955ab19f
chaosannals <chaosannals@outlook.com>
2023-09-19 17:57:45 +0800

    save

--- aliossc/aliosst/src/main.cpp
+++ aliossc/aliosst/src/main.cpp
@@ -1,30 +1,60 @@
 #include <fstream>
 #include <alibabacloud/oss/OssClient.h>
+#include <json/json.h>
 
 using namespace AlibabaCloud::OSS;
 
 // 引入阿里的这个库后 Debug 模式下直接就没了。 Release 下运行正常。
 int main(int argc, char* argv[])
 {
+    Json::Value root;
+
+    std::fstream confJson("Config.json");
+    if (!confJson.good()) {
+        confJson.swap(std::fstream("Config.example.json"));
+    }
+
+    confJson >> root;
+
+
     /* 初始化OSS账号信息。*/
 
     /* yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。*/
-    std::string Endpoint = "oss-cn-shanghai.aliyuncs.com";
+    std::string Endpoint = root.get("endPoint", "aaa").asString();
+    
 
-    //std::cout << "start" << std::endl;
+    std::cout << "endPoint:" << Endpoint << std::endl;
+
+    std::string keyId;
+    std::string keySecret;
+    std::string bucket;
+    std::string target;
+    std::string source;
+
+    if (argc < 6) {
+        keyId = root.get("keyId", "keyId").asString();
+        keySecret = root.get("keySecret", "keySecret").asString();
+        bucket = root.get("bucket", "bucket").asString();
+        target = root.get("target", "target").asString();
+        source = root.get("source", "source").asString();
+    }
+    else {
+        keyId = argv[1];
+        keySecret = argv[2];
+        bucket = argv[3];
+        target = argv[4];
+        source = argv[5];
+    }
+    std::cout << "keyId:" << keyId << std::endl;
+    std::cout << "keySecret:" << keySecret << std::endl;
+    std::cout << "bucket:" << bucket << std::endl;
+    std::cout << "target:" << target << std::endl;
+    std::cout << "source:" << source << std::endl;
 
     /* 初始化网络等资源。*/
     InitializeSdk();
 
     ClientConfiguration conf;
-    /* 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。*/
-    
-    auto keyId = argv[1];
-    auto keySecret = argv[2];
-    auto bucket = argv[3];
-    auto target = argv[4];
-    auto source = argv[5];
-
     auto credentialsProvider = std::make_shared<SimpleCredentialsProvider>(
         keyId,
         keySecret
@@ -45,7 +75,7 @@ int main(int argc, char* argv[])
         std::cout << message << std::endl;
     }
 
-    //std::cin.get();
+    std::cin.get();
 
     /* 释放网络等资源。*/
     ShutdownSdk();

dca2d1f7f3737c30223acb9380bc961779f5132b
chaosannals <chaosannals@outlook.com>
2023-09-27 11:48:47 +0800

    save

---
+++ gnuwin32/woco/src/lex.h
@@ -0,0 +1,7 @@
+#ifndef LEX_H
+
+
+// #define YY_USE_PROTOS
+// #define YY_DECL int yylex()
+
+#endif
--- gnuwin32/woco/src/main.cpp
+++ gnuwin32/woco/src/main.cpp
@@ -1,5 +1,51 @@
 #include <iostream>
+#include "lex.h"
 
-int main() {
+extern "C" int yylex();
+extern "C" void yyrestart(FILE * input_file);
+
+extern "C" int chars;
+extern "C" int words;
+extern "C" int lines;
+
+extern "C" int totchars;
+extern "C" int totwords;
+extern "C" int totlines;
+
+int main(int argc, char** argv) {
+    std::cout << "start count." << std::endl;
+
+    if (argc < 2) {
+        yylex();
+        printf("%8d%8d%8d\n", lines, words, chars);
+        return 0;
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        std::cout << "open: " << argv[i] << std::endl;
+
+        FILE* f = fopen(argv[i], "r");
+        if (!f) {
+            perror(argv[i]);
+            return 1;
+        }
+        yyrestart(f);
+        yylex();
+        fclose(f);
+
+        std::cout << "lines: " << lines << std::endl;
+        printf("%8d%8d%8d %s\n", lines, words, chars, argv[i]);
+        totchars += chars;
+        chars = 0;
+        totwords += words;
+        words = 0;
+        totlines += lines;
+        lines = 0;
+    }
+
+    if (argc > 1) {
+        printf("%8d%8d%8d total\n", totlines, totwords, totchars);
+    }
+
     return 0;
 }

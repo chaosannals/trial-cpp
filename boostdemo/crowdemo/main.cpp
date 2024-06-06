f9b1215cf34430d38c97523365ec5fc6f4a03649
chaosannals <chaosannals@outlook.com>
2024-06-06 15:28:26 +0800

    save

--- boostdemo/crowdemo/main.cpp
+++ boostdemo/crowdemo/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <crow.h>
 
 int main() {
@@ -6,6 +6,33 @@ int main() {
 
     CROW_ROUTE(app, "/")([]() {
         return "Hello world";
+    });
+
+    CROW_ROUTE(app, "/json")([] {
+        crow::json::wvalue x({ {"message", "Hello, World!"} });
+        x["message2"] = "Hello, World.. Again!";
+        return x;
+    });
+
+    // 这个宏有做类型检查，用常量表达式写了个语法分析，有点吊。
+    // 支持 int uint float double  str string path
+    CROW_ROUTE(app, "/hello/<int>/<string>")([](int count, const std::string &other) {
+        if (count > 100)
+            return crow::response(400);
+        std::ostringstream os;
+        os << count << " bottles of beer!";
+        return crow::response(os.str());
+    });
+
+
+    CROW_ROUTE(app, "/add_json").methods("POST"_method)([](const crow::request& req) {
+        auto x = crow::json::load(req.body);
+        if (!x)
+            return crow::response(crow::status::BAD_REQUEST); // same as crow::response(400)
+        int sum = x["a"].i() + x["b"].i();
+        std::ostringstream os;
+        os << sum;
+        return crow::response{ os.str() };
     });
 
     app.port(18080).multithreaded().run();

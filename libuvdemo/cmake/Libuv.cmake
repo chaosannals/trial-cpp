1079080e984af6867782aff70897470e258c6d0f
chaosannals <chaosannals@outlook.com>
2024-06-15 13:57:19 +0800

    save

---
+++ libuvdemo/cmake/Libuv.cmake
@@ -0,0 +1,17 @@
+﻿include(FetchContent)
+
+function(FetchLibuv)
+    FetchContent_Declare(
+        libuv
+        GIT_REPOSITORY git@github.com:libuv/libuv.git
+        GIT_TAG v1.48.0 # 
+        GIT_SHALLOW ON # 浅克隆，git 参数加 --depth=1
+        GIT_PROGRESS ON # 打印下载进度
+    )
+    FetchContent_MakeAvailable(libuv)
+    set(LIBUV_SRC_DIR "${libuv_SOURCE_DIR}")
+    set(LIBUV_SRC_DIR "${libuv_SOURCE_DIR}" PARENT_SCOPE)
+    message(STATUS "[libuv] ${LIBUV_SRC_DIR}")
+
+
+endfunction()
---
+++ libuvdemo/simple/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <uv.h>
+
+int main() {
+    uv_loop_t* loop = reinterpret_cast<uv_loop_t*>(std::malloc(sizeof(uv_loop_t)));
+    uv_loop_init(loop);
+
+    std::cout << "Now quitting." << std::endl;
+    uv_run(loop, UV_RUN_DEFAULT);
+
+    uv_loop_close(loop);
+    free(loop);
+    std::cin.get();
+    return 0;
+}
